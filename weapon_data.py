054664d2d485c756276390866b96987092a876c7
Brian Tran <briant@cpp.edu>
2025-03-04 14:36:51 -0800

    Weapons Library

---
+++ weapon_data.py
@@ -0,0 +1,24 @@
+#weapon_data.py
+
+weapons = {
+    "Scrimitar":{
+        "base_damage": 45,
+        "upgrade_scaling": 5,
+        "requirements": {"Strength": 10, "Dexterity":
+        12},
+        "weight": 4.5,
+        "critical_multiplier": 1.2,
+        "attack_speed": "fast",
+        "special_effects": None
+    },
+    "Battle Axe": {
+        "base_damage": 55
+        "upgrade_scaling": 6,
+        "requirements": {"Strength": 16, "Dexterity": 9},
+        "weight": 7.0,
+        "critical_multiplier": 1.1,
+        "attack_speed": "medium",
+        "special_effects": None
+    },
+
+    
