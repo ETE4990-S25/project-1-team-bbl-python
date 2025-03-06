1e7296a327d97cdf3a3e6a1d464d5fc56e04b5d8
bryandelion <you@example.com>
2025-03-06 09:21:14 -0800

    armor_LibUpdates

---
+++ armor_data.py
@@ -0,0 +1,25 @@
+#Armorlib
+
+armor = {
+    #Warrior Armor 
+    "Warrior Gauntlets": {"defense":6, "weight": 3.0, "type": "light", "dropped_by":"Warrior (Starting)"}, 
+    "Warrior Greaves": {"defense": 6, "weight": 3.0, "type": "light", "dropped_by": "Warrior (Starting)"},
+    "Blue Cowl": {"defense": 5, "weight": 2.5, "type": "light", "dropped_by": "Warrior (Starting)"},
+    "Blue Cloth Vest": {"defense": 7, "weight": 4.0, "type" :"light","dropped_by": "Warrior (Starting)"},    
+    
+    #Hero Starting Armor 
+    "Champion Headband": {"defense": 6, "weight": 2.5, "type": "light", "dropped_by": "Hero (Starting)"},  
+    "Champion Headband": {"defense": 12, "weight": 6.0, "type": "medium", "dropped_by": "Hero (Starting)"},  
+    "Champion Bracers": {"defense": 6, "weight": 3.0, "type": "light", "dropped_by": "Hero (Starting)"},
+    "Champion Gaiters": {"defense": 10, "weight": 5.5, "type": "medium", "dropped_by": "Hero (Starting)"},
+    
+    #Astrologer Starting Armor
+    "Astrologer Hood": {"defense": 4, "weight": 2.0, "type": "light", "dropped_by": "Astrologer (Starting)"},         
+    "Astrologer Rober": {"defense": 6, "weight": 3.5, "type": "light", "dropped_by": "Astrologer (Starting)"},
+    "Astrologer Gloves": {"defense": 5, "weight": 2.5, "type": "light", "dropped_by": "Astrologer (Starting)"},
+    "Astrologer Trousers": {"defense": 5, "weight": 3.0, "type": "light", "dropped_by": "Astrologer (Starting)"},
+    
+    #Prophet Starting Armor 
+    "Prophet Blindfold": {"defense": 4, "weight": 2.0, "type": "light", "dropped_by": "Prophet (Starting)"},
+    "Prophet Robe": {"defense": 6, "weight": 3.5, "type": "light", "dropped_by": "Prophet (Starting)"},
+    "Prophet Trousers": {"defense": 5, "weight": 3.0, "type": "light", "dropped_by": "Prophet (Starting)"},
